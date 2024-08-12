4f5171abefa70c8376e011adfb0f2715ec736305
RC <rckashyapcpe@gmail.com>
2024-08-12 16:50:17 +0800

    Add onclick functions for nav-search__button

---
+++ js/blocks/nav.js
@@ -0,0 +1,13 @@
+let navContainer = document.getElementsByClassName("search-modal")[0];
+
+function showSearchModal() {
+
+    navContainer.style.display = "block";
+
+}
+
+function hideSearchModal() {
+
+    navContainer.style.display = "none";
+    
+}
