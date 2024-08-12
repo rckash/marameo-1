4a1377976085bf975532d92e53709121c7568bfd
RC <rckashyapcpe@gmail.com>
2024-08-12 17:51:26 +0800

    Update nav button functions

--- js/blocks/nav.js
+++ js/blocks/nav.js
@@ -1,13 +1,18 @@
 let navContainer = document.getElementsByClassName("search-modal")[0];
+let mobileNavDropdown = document.getElementsByClassName("mobile-nav-dropdowns__container")[0];
 
 function showSearchModal() {
-
     navContainer.style.display = "block";
-
 }
 
 function hideSearchModal() {
-
     navContainer.style.display = "none";
-    
+}
+
+function showMobileNavDropdown() {
+    mobileNavDropdown.style.display = "block";
+}
+
+function hideMobileNavDropdown() {
+    mobileNavDropdown.style.display = "none";
 }
